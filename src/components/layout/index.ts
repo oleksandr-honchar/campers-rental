ed72fccc14554ecc62fccd4ea034b43ce0d00f15
Oleksandr Honchar <figules@gmail.com>
2025-12-07 10:43:54 +0100

    fix: Type error due to default vs named export in layout index

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,4 +1,3 @@
-export { Header } from './Header';
-export { Footer } from './Footer';
+export { default as Header } from './Header';
 export { Container } from './Container';
 export { Section } from './Section';
